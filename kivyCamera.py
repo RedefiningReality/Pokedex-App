7f626a2a5911305d0a842e17fee198cd69e70601
funkmonk9999 <elliotwillming53@gmail.com>
2022-10-18 18:48:26 -0500

    Update kivyCamera.py

--- kivyCamera.py
+++ kivyCamera.py
@@ -1,7 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-import time
 
 Builder.load_string('''
 <CameraClick>:
@@ -26,8 +25,7 @@ Builder.load_string('''
 class CameraClick(BoxLayout):
     def capture(self):
         camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
+        camera.export_to_png("pokemonImage")
         print("Captured")
 
 
